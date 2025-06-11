3c387e1795379248609861c61efa74c9c67e3943
div <div490@mail.ru>
2025-06-11 22:22:53 +0700

    styles
    Подробнее:
    - Красная строка
    - адаптивные отступ
    - top куба
    - uppercase

--- public/js/index.js
+++ public/js/index.js
@@ -1,6 +1,4 @@
-let toBeContinue = document.querySelector(".continue")
 let menu = document.querySelector(".menu")
-let continueState = 1
 
 menu.onmousedown = (e) => {
     e.target.classList.toggle('menu-item-selected')
@@ -9,13 +7,5 @@ menu.onmouseup = (e) => {
     e.target.classList.toggle('menu-item-selected')
 }
 
-toBeContinue.onclick = () => {
-    if (continueState === 1) {
-        toBeContinue.textContent = 'to be continue...'
-        continueState = 2
-    } else {
-        toBeContinue.textContent = 'Продолжение следует...'
-        continueState = 1
-    }
-}
+
 
